0394dd114fe6738cb1dfde702f99113df56e00f5
mohit0709 <mohitaglawe16@gmail.com>
2024-06-24 16:38:13 +0530

    app.js is updated

--- starter/app.js
+++ starter/app.js
@@ -1,1 +1,25 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+
+
+//route
+app.get('/hello', (req,res) => {
+
+    res.send('Task Mangaer App');
+});
+
+//General Info:
+//app.get('/api/v1/tasks')  --this one gets all the tasks
+//app.post('/apiv1/tasks')  --this one creates a new task
+//app.get('/api/v1/tasks/:id')  --this one gets a single task
+//app.patch('/api/v1/tasks/:id')--this one updates the task
+//app.delete('/api/v1/tasks/:id')--this one deletes the task
+
+app.use('/api/v1/tasks', tasks);
+
+const port = 3000;
+
+app.listen(port, console.log(`Server is listening on port ${port}...`));
+
+
