1f50d60a833b79bd825543e4ad8090afa67f3a34
mohit0709 <mohitaglawe16@gmail.com>
2024-06-29 16:38:12 +0530

    tasks.js filed updated

--- starter/routes/tasks.js
+++ starter/routes/tasks.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.route('/').get((req,res) => {
-    res.send('all items');
-});
+const {getAllTasks,getTask,createTask,updateTask,deleteTask} = require('../controllers/tasks');
+
+router.route('/').get(getAllTasks).post(createTask);
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
 
 module.exports = router;
