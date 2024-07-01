b531015959259b89283b63ba34b28a4352ea0682
mohit0709 <mohitaglawe16@gmail.com>
2024-07-02 01:07:13 +0530

    tasks.js file updated

--- starter/controllers/tasks.js
+++ starter/controllers/tasks.js
@@ -3,8 +3,9 @@ const getAllTasks = (req,res) => {
     res.send('get all tasks');
 }
 
-const createTask = (req,res) => {
-    res.json(req.body);
+const createTask = async (req,res) => {
+    const task = await Task.create(req.body)
+    res.status(201).json({ task });
 }
 
 const getTask = (req,res) => {
