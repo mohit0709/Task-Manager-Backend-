f2e84b94411a8ef161491e7a578aa83a9d054a74
mohit0709 <mohitaglawe16@gmail.com>
2024-07-02 15:00:41 +0530

    model folder updated

--- starter/model/Task.js
+++ starter/model/Task.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    name:String,
-    completed:Boolean
+    name:{
+        //validators for schema
+        type:String,
+        required:[true,'name is must'],
+        trim:true,
+        maxlength:[20,'name cannot exceed 20 characters'],
+    },
+    completed:{
+        type:Boolean,
+        default:false,
+    }
 })
 
 module.exports = mongoose.model('Task', TaskSchema);
